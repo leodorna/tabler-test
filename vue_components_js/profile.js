f8e27ece54bdb3243eda19aae74902152592a44a
leodorna <leo.dorneles@hotmail.com>
2021-12-28 01:51:19 -0300

    add profile page

--- vue_components_js/profile.js
+++ vue_components_js/profile.js
@@ -5,7 +5,9 @@ const ProfilePage = Vue.component("profile-page", {
     data: function(){
         return {
             user: this.$root.user,
-            initials: this.get_initials()
+            initials: this.get_initials(),
+            enableSave: false,
+            success: null
         }
     },
     mounted: async function (){
@@ -16,6 +18,36 @@ const ProfilePage = Vue.component("profile-page", {
         get_initials: function(){
             const names_array = this.$root.user.name.split(' ')
             return String(names_array[0][0] + names_array[names_array.length-1][0]).toUpperCase()
+        },
+
+        edit_section: function(e){
+            
+            e.target.closest(".section-profile")
+                    .querySelectorAll("input")
+                    .forEach( 
+                        d => {
+                            d.removeAttribute('disabled')
+                        }
+                    )
+
+        },
+
+        save: async function(){
+            const request = await session.patchRequest('users/update/', this.user)
+            let data = await request.json()
+
+            if(request.ok){
+                this.success = true
+            } else {
+                this.success = false
+            }
+
+            parent = this
+
+            setTimeout(function(){
+                parent.success = null
+            }, 6000)
         }
+
     }
 })
